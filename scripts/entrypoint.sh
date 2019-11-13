4e5d6597ff9c40b8a6296c7f9874fb4b15e1c1d2
Tony Skidmore <tony@skidmore.co.uk>
2019-11-13 20:58:55 +0000

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# eval $(ssh-agent -s)
+# echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 cd /playbooks
 
 git clone https://github.com/tonyskidmore/cloudtools-content.git
+
+ls cloudtools-content/
+echo $ANSIBLE_PLAYBOOK
 
 ansible-playbook $ANSIBLE_PLAYBOOK $ANSIBLE_PARAMS -i /inventory
